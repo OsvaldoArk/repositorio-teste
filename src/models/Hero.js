d88e3371e03b9587ec6f18fa6b167da70aab86ea
osvaldo arruda <osvaldo.ark@gmail.com>
2025-09-03 18:50:16 -0300

    feat: adicionando o model do heroi

---
+++ src/models/Hero.js
@@ -0,0 +1,11 @@
+export class Hero{
+
+    constructor(id,name,health,energy,job,skills){
+        this.id = id
+        this.name = name
+        this.health = health
+        this.energy = energy
+        this.job = job 
+        this.skills = skills
+    }
+}
